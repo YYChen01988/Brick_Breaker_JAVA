9c3fd136d100a34be49de130f663348ffe05eb41
YingYing <chen.yingying.01988@gmail.com>
2018-11-12 13:34:38 +0000

    ball is able to bounce around, added mapGenerator class

--- src/BrickBreaker/GamePlay.java
+++ src/BrickBreaker/GamePlay.java
@@ -6,7 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.util.Timer;
+import javax.swing.Timer;
 
 public class GamePlay extends JPanel implements KeyListener, ActionListener {
 
@@ -26,8 +26,8 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
         addKeyListener(this);
         setFocusable(true);
         setFocusTraversalKeysEnabled(false);
-//        timer = new Timer(delay, this);
-//        timer.start();
+        timer = new Timer(delay, this);
+        timer.start();
     }
 
 
@@ -44,17 +44,38 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
 
         //paddle
         g.setColor(Color.green);
-        g.fillRect(playerX, 550, 100, 8);
+        g.fillRect(playerX, 550, 150, 8);
 
         //Ball
         g.setColor(Color.yellow);
-        g.fillRect(ballPositionX, ballPositionY, 20, 20);
+        g.fillOval(ballPositionX, ballPositionY, 20, 20);
 
+        g.dispose();
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        timer.start();
+        if(play){
 
+            if(new Rectangle(ballPositionX, ballPositionY, 20, 20).intersects(new Rectangle(playerX, 550, 100, 8))){
+                ballYDirection = -ballYDirection;
+            }
+
+            ballPositionX += ballXDirection;
+            ballPositionY += ballYDirection;
+            if (ballPositionX < 0 ){
+                ballXDirection = -ballXDirection;
+            }
+            if (ballPositionY < 0 ){
+                ballYDirection = -ballYDirection;
+            }
+            if (ballPositionX < 670 ){
+                ballXDirection = -ballXDirection;
+            }
+        }
+
+        repaint();
     }
 
     @Override
